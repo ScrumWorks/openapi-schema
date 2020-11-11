e6cddaf8e2b638c2ac35add3326dcd5e41fdc482
Tomas Lang <tomas.lang@gmail.com>
2020-11-11 13:59:14 +0100

    Add declaration of strict types to all files

--- src/OpenApiTranslatorInterface.php
+++ src/OpenApiTranslatorInterface.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace ScrumWorks\OpenApiSchema;
 
 use ScrumWorks\OpenApiSchema\ValueSchema\ValueSchemaInterface;
--- src/ValueSchema/ValueSchemaInterface.php
+++ src/ValueSchema/ValueSchemaInterface.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace ScrumWorks\OpenApiSchema\ValueSchema;
 
 interface ValueSchemaInterface
