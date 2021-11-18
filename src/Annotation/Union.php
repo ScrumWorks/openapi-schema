1eb45c986c320b8cf07fc01b7bd69ca86072e2a9
Tomas Votruba <tomas.vot@gmail.com>
2021-11-18 12:46:16 +0300

    make Union dual attribute/annotation

--- src/Annotation/Union.php
+++ src/Annotation/Union.php
@@ -5,28 +5,25 @@ declare(strict_types=1);
 namespace ScrumWorks\OpenApiSchema\Annotation;
 
 use Attribute;
+use Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor;
 
 /**
  * @Annotation
  * @Target({"PROPERTY", "ANNOTATION"})
  *
- * Doctrine annotations reader uses `@var` annotations and doesn't know nullability
+ * @NamedArgumentConstructor()
  */
 #[Attribute(Attribute::TARGET_PROPERTY | Attribute::TARGET_CLASS)]
 final class Union implements ValueInterface
 {
     /**
-     * @var string
+     * @param array<string, string>|null $mapping
+     * @param \ScrumWorks\OpenApiSchema\Annotation\ValueInterface[] $types we can't FQN, because shitty doctrine annotations
      */
-    public ?string $discriminator = null;
-
-    /**
-     * @var array<string, string>
-     */
-    public ?array $mapping = null;
-
-    /**
-     * @var \ScrumWorks\OpenApiSchema\Annotation\ValueInterface[] we can't FQN, because shitty doctrine annotations
-     */
-    public ?array $types = null;
+    public function __construct(
+        public array $types = [],
+        public array|null $mapping = null,
+        public string|null $discriminator = null,
+    ) {
+    }
 }
