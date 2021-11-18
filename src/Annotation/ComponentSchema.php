85ffabd0ee0e42b066203e5167200c9a4de121ba
Tomas Votruba <tomas.vot@gmail.com>
2021-11-18 12:23:54 +0300

    make ComponentSchema dual attribute/annotation

--- src/Annotation/ComponentSchema.php
+++ src/Annotation/ComponentSchema.php
@@ -5,18 +5,20 @@ declare(strict_types=1);
 namespace ScrumWorks\OpenApiSchema\Annotation;
 
 use Attribute;
+use Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor;
 
 /**
  * @Annotation
  * @Target("CLASS")
  *
  * Doctrine annotations reader uses `@var` annotations and doesn't know nullability
+ * @NamedArgumentConstructor()
  */
 #[Attribute(Attribute::TARGET_CLASS)]
 final class ComponentSchema implements ValueInterface
 {
-    /**
-     * @var string
-     */
-    public ?string $schemaName = null;
+    public function __construct(
+        public string $schemaName
+    ) {
+    }
 }
