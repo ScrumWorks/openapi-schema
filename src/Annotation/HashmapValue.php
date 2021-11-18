d5f109d97415f78ab787bdac4b346ed47718169f
Tomas Votruba <tomas.vot@gmail.com>
2021-11-18 12:29:14 +0300

    make HashmapValue dual attribute/annotation

--- src/Annotation/HashmapValue.php
+++ src/Annotation/HashmapValue.php
@@ -5,23 +5,23 @@ declare(strict_types=1);
 namespace ScrumWorks\OpenApiSchema\Annotation;
 
 use Attribute;
+use Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor;
 
 /**
  * @Annotation
  * @Target({"PROPERTY", "ANNOTATION"})
  *
- * Doctrine annotations reader uses `@var` annotations and doesn't know nullability
+ * @NamedArgumentConstructor()
  */
 #[Attribute(Attribute::TARGET_PROPERTY | Attribute::TARGET_CLASS)]
 final class HashmapValue implements ValueInterface
 {
     /**
-     * @var string[]
+     * @param string[] $requiredProperties
      */
-    public array $requiredProperties = [];
-
-    /**
-     * @var \ScrumWorks\OpenApiSchema\Annotation\ValueInterface we can't FQN, because shitty doctrine annotations
-     */
-    public ?ValueInterface $itemsSchema = null;
+    public function __construct(
+        public array $requiredProperties,
+        public ValueInterface $itemsSchema,
+    ) {
+    }
 }
