e8dba1fe944b4025ffbb7b21acabe572f9ba2149
Tomas Votruba <tomas.vot@gmail.com>
2021-11-18 12:26:12 +0300

    make ArrayValue dual attribute/annotation

--- src/Annotation/ArrayValue.php
+++ src/Annotation/ArrayValue.php
@@ -5,33 +5,21 @@ declare(strict_types=1);
 namespace ScrumWorks\OpenApiSchema\Annotation;
 
 use Attribute;
+use Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor;
 
 /**
  * @Annotation
  * @Target({"PROPERTY", "ANNOTATION"})
- *
- * Doctrine annotations reader uses `@var` annotations and doesn't know nullability
+ * @NamedArgumentConstructor()
  */
 #[Attribute(Attribute::TARGET_PROPERTY | Attribute::TARGET_CLASS)]
 final class ArrayValue implements ValueInterface
 {
-    /**
-     * @var int
-     */
-    public ?int $minItems = null;
-
-    /**
-     * @var int
-     */
-    public ?int $maxItems = null;
-
-    /**
-     * @var bool
-     */
-    public ?bool $uniqueItems = null;
-
-    /**
-     * @var \ScrumWorks\OpenApiSchema\Annotation\ValueInterface we can't FQN, because shitty doctrine annotations
-     */
-    public ?ValueInterface $itemsSchema = null;
+    public function __construct(
+        public int $maxItems,
+        public ValueInterface $itemsSchema,
+        public int|null $minItems = null,
+        public bool $uniqueItems = false,
+    ) {
+    }
 }
