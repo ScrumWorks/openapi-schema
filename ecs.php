927f8c78cf605f4d6281f54dd098829c88123bd0
Adam Lutka <adam.lutka@cncenter.cz>
2021-11-18 13:32:13 +0100

    Upgrade dev tools

--- ecs.php
+++ ecs.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\Basic\Psr4Fixer;
-use PhpCsFixer\Fixer\Casing\NativeFunctionCasingFixer;
 use PhpCsFixer\Fixer\FunctionNotation\NativeFunctionInvocationFixer;
 use PhpCsFixer\Fixer\Import\FullyQualifiedStrictTypesFixer;
 use PhpCsFixer\Fixer\Import\GlobalNamespaceImportFixer;
@@ -15,10 +13,8 @@ use PhpCsFixer\Fixer\Strict\StrictComparisonFixer;
 use PhpCsFixer\Fixer\Strict\StrictParamFixer;
 use PhpCsFixer\Fixer\Whitespace\IndentationTypeFixer;
 use PhpCsFixer\Fixer\Whitespace\MethodChainingIndentationFixer;
-use SlevomatCodingStandard\Helpers\PropertyTypeHint;
 use SlevomatCodingStandard\Sniffs\Arrays\DisallowImplicitArrayCreationSniff;
 use SlevomatCodingStandard\Sniffs\Classes\DisallowLateStaticBindingForConstantsSniff;
-use SlevomatCodingStandard\Sniffs\Classes\UnusedPrivateElementsSniff;
 use SlevomatCodingStandard\Sniffs\Classes\UselessLateStaticBindingSniff;
 use SlevomatCodingStandard\Sniffs\ControlStructures\RequireNullCoalesceOperatorSniff;
 use SlevomatCodingStandard\Sniffs\Exceptions\DeadCatchSniff;
@@ -30,15 +26,12 @@ use SlevomatCodingStandard\Sniffs\Namespaces\UseFromSameNamespaceSniff;
 use SlevomatCodingStandard\Sniffs\PHP\OptimizedFunctionsWithoutUnpackingSniff;
 use SlevomatCodingStandard\Sniffs\PHP\UselessParenthesesSniff;
 use SlevomatCodingStandard\Sniffs\PHP\UselessSemicolonSniff;
-use SlevomatCodingStandard\Sniffs\TypeHints\PropertyTypeHintSniff;
 use SlevomatCodingStandard\Sniffs\Variables\DuplicateAssignmentToVariableSniff;
-use SlevomatCodingStandard\Sniffs\Variables\UnusedVariableSniff;
 use SlevomatCodingStandard\Sniffs\Variables\UselessVariableSniff;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\CodingStandard\Fixer\ArrayNotation\StandaloneLineInMultilineArrayFixer;
 use Symplify\CodingStandard\Fixer\Commenting\ParamReturnAndVarTagMalformsFixer;
 use Symplify\CodingStandard\Fixer\LineLength\LineLengthFixer;
-use Symplify\CodingStandard\Fixer\Spacing\RemoveSpacingAroundModifierAndConstFixer;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
 # see https://github.com/symplify/easy-coding-standard
@@ -54,10 +47,11 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         # pick anything from https://github.com/symplify/easy-coding-standard#use-prepared-checker-sets
         SetList::PSR_12,
         SetList::COMMON,
-        SetList::PHP_70,
-        SetList::PHP_71,
         SetList::CLEAN_CODE,
-        SetList::DEAD_CODE,
+        SetList::ARRAY,
+        SetList::STRICT,
+        SetList::SYMPLIFY,
+        SetList::CONTROL_STRUCTURES,
     ]);
 
     $parameters->set('skip', [
@@ -83,16 +77,12 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         StrictParamFixer::class => null,
         # breaks code
         ReferenceThrowableOnlySniff::class . '.' . ReferenceThrowableOnlySniff::CODE_REFERENCED_GENERAL_EXCEPTION => null,
-        Psr4Fixer::class => null,
         UnusedUsesSniff::class . '.' . UnusedUsesSniff::CODE_MISMATCHING_CASE => [
             __DIR__ . '/tests/*',
         ],
     ]);
 
     $services = $containerConfigurator->services();
-
-    // @todo fix
-    // $services->set(ProtectedToPrivateFixer::class);
 
     $services->set(MethodChainingIndentationFixer::class);
 
@@ -125,14 +115,14 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     # make @param, @return and @var format united
     $services->set(ParamReturnAndVarTagMalformsFixer::class);
 
-    # spaces
-    $services->set(RemoveSpacingAroundModifierAndConstFixer::class);
-
     # use 4 spaces to indent
     $services->set(IndentationTypeFixer::class);
 
     # native functions should be casted in lowercase
-    $services->set(NativeFunctionCasingFixer::class);
+    $services->set(NativeFunctionInvocationFixer::class)
+        ->call('configure', [[
+            'include' => [NativeFunctionInvocationFixer::SET_ALL]
+        ]]);
 
     # import namespaces
     $services->set(FullyQualifiedStrictTypesFixer::class);
